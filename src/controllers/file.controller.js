3cac0758403a1459c343587b0103662a11d5f9b3
Alessandro Mangiameli <alessandro.mangiameli@arcww.leoburnett.it>
2017-05-19 16:23:07 +0200

    add httpstatus module

--- src/controllers/file.controller.js
+++ src/controllers/file.controller.js
@@ -3,6 +3,7 @@ import fs from 'fs';
 import url from 'url';
 import dotenv from 'dotenv';
 import config from './../config/app.config';
+import HTTPStatus from 'http-status';
 
 
 function load(req, res, next) {
@@ -11,14 +12,14 @@ function load(req, res, next) {
 
 	jwt.verify(token, config.jwtSecretKey, (err, decoded) => {
 		if(err) {
-			res.json(err);
+			res.json(err, HTTPStatus.UNAUTHORIZED);
 			return;
 		}
 
 		fs.readFile(file, 'utf8', (err, data) => {
 			if (err) throw err;
 			res.setHeader('content-type', 'text/javascript');
-			res.send(data);
+			res.send(data, HTTPStatus.OK);
 		});
 	});
 }
